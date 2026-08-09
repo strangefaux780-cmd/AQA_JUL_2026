214784f7ccf87e2def89e74f8cbb6eb0b59f60ba
adminus <strangefaux780@gmail.com>
2026-08-09 20:38:45 +0300

    Homework2masive.Oleksandr.Pugach.

---
+++ src/org/prog/session3/Homework2masive.java
@@ -0,0 +1,23 @@
+package org.prog.session3;
+
+import java.lang.module.FindException;
+import java.util.Random;
+
+public class Homework2masive {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr1 = new int[7];
+        int[] arr2 = new int[7];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = random.nextInt(100);
+            System.out.println(arr1[i]);
+        }
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[arr2.length - 1 - i] = arr1[i];
+            }
+            for (int i = 0;i< arr2.length;i++){
+                System.out.println(arr2[i]);
+            }
+
+    }
+}
