2b20cd75a25a8a56d3be5178de2e0ad0c0476616
adminus <strangefaux780@gmail.com>
2026-08-17 02:55:29 +0300

    Homework4myproject

--- src/org/prog/session5/MainB.java
+++ src/org/prog/session5/MainB.java
@@ -7,19 +7,19 @@ public class MainB {
         Route route4=new Route("Khmelnitskiy-Krasilov",40);;
         Route route5=new Route("Khmelnitskiy-Stara-Sinyava",65);
         BUSHM bus1 = new BUSHM();
-        bus1.numberBus = "Spinter-1";
+        bus1.numberBus = "Spinter-BX2327RT";
         bus1.goRouteB(route1);
         BUSHM bus2 = new BUSHM();
-        bus2.numberBus="Crafter-1";
+        bus2.numberBus="Crafter-BX6754QW";
         bus2.goRouteB(route2);
         BUSHM bus3=new BUSHM();
-        bus3.numberBus="Sprinter-2";
+        bus3.numberBus="Sprinter-BX9876YT";
         bus3.goRouteB(route3);
         BUSHM bus4 =new BUSHM();
-        bus4.numberBus="Crafter-2";
+        bus4.numberBus="Crafter-BX7809PO";
         bus4.goRouteB(route4);
         BUSHM bus5=new BUSHM();
-        bus5.numberBus="Renault-1";
+        bus5.numberBus="Renault-BX9087YT";
         bus5.goRouteB(route5);
 
 
