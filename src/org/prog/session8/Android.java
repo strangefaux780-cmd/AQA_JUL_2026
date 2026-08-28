68267618b184bfb4ceb548b4ce90ec743669e6d7
adminus <strangefaux780@gmail.com>
2026-08-28 18:35:19 +0300

    HomeworkCOLORMODEL

--- src/org/prog/session8/Android.java
+++ src/org/prog/session8/Android.java
@@ -1,8 +1,26 @@
 package org.prog.session8;
 
 public class Android implements Iphone {
+    String color;
+    String model;
+    public Android (String color,String model) {
+        this.color = color;
+        this.model = model;
+    }
+        @Override
+        public boolean equals(Object Obj){
+            if(Obj instanceof Android){
+                Android andr=(Android) Obj;
+                boolean isequals=this.color.equals(andr.color)
+                        && this.model.equals(andr.model);
+                return isequals;
+            }
+            return false;
+        }
+
     @Override
     public void unklockPhone(){
         System.out.println("android uncklock fingers ");
     }
+    public int hashCode(){return (this.color+this.model).hashCode();}
 }
--- src/org/prog/session8/Apple.java
+++ src/org/prog/session8/Apple.java
@@ -1,10 +1,34 @@
 package org.prog.session8;
 
+import org.prog.session9.Car;
+
 public class Apple implements Iphone {
-    @Override
-    public void unklockPhone(){
-        System.out.println("apple unklock face id ");
+    String color;
+    String model;
+
+    public Apple(String color, String model) {
+        this.model = model;
+        this.color = color;
+    }
+    public boolean equals(Object obj){
+        if (obj instanceof Apple){
+            Apple apel=(Apple) obj;
+            boolean isequals=this.color.equals(apel.color)
+                    && this.model.equals(apel.model);
+
+        }
+        return false;
     }
 
+    @Override
+    public void unklockPhone() {
+        System.out.println("apple unklock face id ");
+            }
+    public int hashCode(){return (this.color+this.model).hashCode();}
+        }
 
-}
+
+
+
+
+
