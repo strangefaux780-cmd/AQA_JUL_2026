f91ba660e6c91d3d2952cb716ff35e004ef85934
adminus <strangefaux780@gmail.com>
2026-08-28 18:35:19 +0300

    HomeworkCOLORMODEL

--- src/org/prog/session8/Main1.java
+++ src/org/prog/session8/Main1.java
@@ -2,12 +2,29 @@ package org.prog.session8;
 
 public class Main1 {
     public static void main(String[] args) {
-        Android android = new Android();
-        Apple apple = new Apple();
-        unklockfingers(android);
-        unklockFaceid(apple);
-        HomeWork.unlockIphone(android);
-        HomeWork.unlockIphone(apple);
+        Android android = new Android("Green","Pixel 10");
+        Android android1 = new Android("black","Samsung Galaxy 26");
+        Android androidanoTher=new Android("Green","Pixel 10");
+        Apple apple = new Apple("White","XS");
+        Apple apple1=new Apple("Golden","XR");
+        Apple appleanoTher=new Apple("white","17 pro max");
+
+      //  unklockfingers(android);
+        //unklockFaceid(apple);
+        //HomeWork.unlockIphone(android);
+        //HomeWork.unlockIphone(apple);
+        System.out.println(android.equals(apple));
+        System.out.println(android.hashCode());
+        System.out.println(android.equals(androidanoTher));
+        System.out.println(android.hashCode());
+        System.out.println(apple.equals(appleanoTher));
+        System.out.println(apple.hashCode());
+        System.out.println(apple1.equals(appleanoTher));
+        System.out.println(apple1.hashCode());
+        System.out.println(android1.equals(android));
+        System.out.println(android1.hashCode());
+
+
 
 
 
