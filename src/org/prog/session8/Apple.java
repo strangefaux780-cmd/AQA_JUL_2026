5322ce2e4034da69e69872b368dd296a42257f05
strangefaux780-cmd <strangefaux780@gmail.com>
2026-09-05 23:33:43 +0300

    HomeworkMAPset

--- src/org/prog/session8/Apple.java
+++ src/org/prog/session8/Apple.java
@@ -5,16 +5,20 @@ import org.prog.session9.Car;
 public class Apple implements Iphone {
     String color;
     String model;
-    public String NumbersPeople;
+    String contacts;
+    String owner;
 
-    public Apple(String color, String model) {
+    public Apple(String color, String model, String contacts, String owner) {
         this.model = model;
         this.color = color;
+        this.contacts = contacts;
+        this.owner = owner;
     }
-    public boolean equals(Object obj){
-        if (obj instanceof Apple){
-            Apple apel=(Apple) obj;
-            boolean isequals=this.color.equals(apel.color)
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Apple) {
+            Apple apel = (Apple) obj;
+            boolean isequals = this.color.equals(apel.color)
                     && this.model.equals(apel.model);
 
         }
@@ -24,9 +28,17 @@ public class Apple implements Iphone {
     @Override
     public void unklockPhone() {
         System.out.println("apple unklock face id ");
-            }
-    public int hashCode(){return (this.color+this.model).hashCode();}
-        }
+    }
+
+    public int hashCode() {
+        return (this.color + this.model + this.contacts + this.owner).hashCode();
+    }
+
+    public String toString() {
+        return "Apple[color=" + color + ", model=" + model + ", contacts=" + contacts + ", owner=" + owner + "]";
+    }
+}
+
 
 
 
--- src/org/prog/session8/Main1.java
+++ src/org/prog/session8/Main1.java
@@ -7,30 +7,68 @@ import java.util.Set;
 
 public class Main1 {
     public static void main(String[] args) {
+        Map<Android,Set<String>> androidmap=new HashMap<>();
+        Android android1=new Android("Black","Pixel 8 Pro","+380 67 123 45 67","David");
+        Android android2=new Android( "White","Pixel 9a","+380 50 987 65 43","Bob");
+        Android android3=new Android("Blue","Huawei","+380 63 456 78 90","Jason");
+        androidmap.put(android1,new HashSet<>());
+        androidmap.get(android1).add("380 67 123 45 67");
+        androidmap.put(android2,new HashSet<>());
+        androidmap.get(android2).add("+380 50 987 65 43");
+        androidmap.put(android3,new HashSet<>());
+        androidmap.get(android3).add("+380 63 456 78 90");
+        Map<Apple,Set<String>>applemap=new HashMap<>();
+        Apple apple1=new Apple("Gold","Iphone 16e","+380 97 321 65 98","David");
+        Apple apple2=new Apple("Red","Iphone 13 Pro Max","+380 95 852 14 73","Jane");
+        Apple apple3=new Apple("Orange","Iphone 17 Pro Max","+38077 77 777 77","Oleksandr");
+        applemap.put(apple1,new HashSet<>());
+        applemap.get(apple1).add("+380 97 321 65 98");
+        applemap.put(apple2,new HashSet<>());
+        applemap.get(apple2).add("+380 95 852 14 73");
+        applemap.put(apple3,new HashSet<>());
+        applemap.get(apple3).add("+38077 77 777 77");
+       // System.out.println(applemap + "(&&)" + androidmap);
+        Map<Android,Set<String>> androidOwner=new HashMap<>();
+        androidOwner.put(android1,new HashSet<>());
+        androidOwner.get(android1).add("David");
+        androidOwner.put(android2,new HashSet<>());
+        androidOwner.get(android2).add("Bob");
+        androidOwner.put(android3,new HashSet<>());
+        androidOwner.get(android3).add("Jason");
+        System.out.println(androidOwner);
+        Map<Apple,Set<String>> appleOwner=new HashMap<>();
+        appleOwner.put(apple1,new HashSet<>());
+        appleOwner.get(apple1).add("David");
+        appleOwner.put(apple2,new HashSet<>());
+        appleOwner.get(apple2).add("Jane");
+        appleOwner.put(apple3,new HashSet<>());
+        appleOwner.get(apple3).add("Oleksandr");
+        System.out.println(appleOwner);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //Android android = new Android("Green","Pixel 10");
         //Android android1 = new Android("black","Samsung Galaxy 26");
        // Android androidanoTher=new Android("Green","Pixel 10");
        // Apple apple = new Apple("White","XS");
        // Apple apple1=new Apple("Golden","XR");
      //   Apple appleanoTher=new Apple("white","17 pro max");
-        Set String=new HashSet<>();
-        Map<String, Set<String>> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        Map<String, Set<String>> map2 = new HashMap<>();
-        map.put("Android" , new HashSet<>());
-        map.get("Android").add("+380 68 456 78 90");
-        map.get("Android").add("+380 73 345 67 89");
-        map.get("Android").add("+380 67 234 56 78");
-        map.put("Apple",new HashSet<>());
-        map.get("Apple").add("+380 50 123 45 67");
-        map.get("Apple").add("+380 67 482 91 53");
-        map.get("Apple").add("+380 55 345 67 85");
-        map2.put("Android",new HashSet<>());
-        map2.get("Android").add("David");
-        map2.put("Apple",new HashSet<>());
-        map2.get("Apple").add("Jane");
-        System.out.println(map);
-        System.out.println(map2);
+
       //  unklockfingers(android);
         //unklockFaceid(apple);
         //HomeWork.unlockIphone(android);
@@ -45,25 +83,17 @@ public class Main1 {
         //System.out.println(apple1.hashCode());
         //System.out.println(android1.equals(android));
         //System.out.println(android1.hashCode());
-
-
-
-
-
     }
-
-    public static void unklockfingers(Android android) {
-        android.unklockPhone();
+ //   public static void unklockfingers(Android android) {
+   //     android.unklockPhone();
     }
-
-    public static void unklockFaceid(Apple apple) {
-        apple.unklockPhone();
-    }
-    public static void unklockIphone(Iphone iphone) {
-        iphone.unklockPhone();
-
-    }
+   // public static void unklockFaceid(Apple apple) {
+     //   apple.unklockPhone();
+    //}
+    //public static void unklockIphone(Iphone iphone) {
+      //  iphone.unklockPhone();
 
 
 
-}
+
+
