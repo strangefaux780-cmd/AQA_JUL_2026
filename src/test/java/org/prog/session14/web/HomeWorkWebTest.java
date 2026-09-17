5788d3277774393c71e84bcca4c358d09f01e0e1
strangefaux780-cmd <strangefaux780@gmail.com>
2026-09-17 16:28:42 +0300

    Test2HoMWoRS

--- src/test/java/org/prog/session14/web/HomeWorkWebTest.java
+++ src/test/java/org/prog/session14/web/HomeWorkWebTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
